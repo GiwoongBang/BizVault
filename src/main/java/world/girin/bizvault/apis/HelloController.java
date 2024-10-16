6ffd2d134ca1ebfd5ac344d365b6db83d18bf54c
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-10-17 00:04:12 +0900

    fix: AWS EC2 502 Bad Gateway Error

--- src/main/java/world/girin/bizvault/apis/HelloController.java
+++ src/main/java/world/girin/bizvault/apis/HelloController.java
@@ -2,14 +2,15 @@ package world.girin.bizvault.apis;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
+    @GetMapping("/")
+    public RedirectView index() {
 
-        return "Security Check";
+        return new RedirectView("/index.html");
     }
 
     @GetMapping("/healthcheck")
